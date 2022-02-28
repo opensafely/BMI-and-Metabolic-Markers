71b4bebcf8d89a1284d3a5a94bc1a753ccdd46ff
Miriam-S-git <miriam.samuel@nhs.net>
2022-02-28 13:03:19 +0000

    2 point decimal

--- analysis/had_bmi_regression.R
+++ analysis/had_bmi_regression.R
@@ -151,7 +151,8 @@ univariate_had_bmi_2020 <- univ_tab_base %>%
   mutate(n_had_bmi = univ_tab_base$'1', .after = term) %>%
   mutate(N_total = (univ_tab_base$'0' + univ_tab_base$'1'), .after = term ) %>%
   mutate(proportion_had_bmi = n_had_bmi/N_total, .after = n_had_bmi) %>%
-  select(term, n_had_bmi,  proportion_had_bmi, estimate, conf.low, conf.high, p.value)
+  select(term, n_had_bmi,  proportion_had_bmi, estimate, conf.low, conf.high, p.value) %>%
+  mutate(across(where(is.numeric), round, digits = 2))
 
 
 
