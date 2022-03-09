83f42e8ff2d5d4f3e618b04b315677ee07c40220
Miriam-S-git <74236842+Miriam-S-git@users.noreply.github.com>
2022-03-09 16:44:58 +0000

    Update had_bmi_regression.R

--- analysis/had_bmi_regression.R
+++ analysis/had_bmi_regression.R
@@ -79,8 +79,8 @@ BMI_complete_categories_2020 <- BMI_complete_categories_2020 %>%
   dplyr::mutate(age_group = fct_relevel(age_group, "18-39", after = 0)) %>%
   dplyr::mutate(ethnic_no_miss = as.factor(ethnic_no_miss)) %>%#
   dplyr::mutate(ethnic_no_miss = fct_relevel(ethnic_no_miss, "White", after = 0)) %>%
-  dplyr::mutate(eth_group_16 = as.factor(ethnic_no_miss)) %>%#
-  dplyr::mutate(eth_group_16 = fct_relevel(ethnic_no_miss, "White", after = 0))
+  dplyr::mutate(eth_group_16 = as.factor(eth_group_16)) %>%#
+  dplyr::mutate(eth_group_16 = fct_relevel(eth_group_16, "White", after = 0))
 
 ## instructions from R for epi
 # Univariate model with age
@@ -170,12 +170,7 @@ univariate_had_bmi_2020 <- univ_tab_base  %>%
 
 
 ######################################################### 
-
-
-
-################################################################################################
-################################################################################################
-#  1.  Univariate 2021
+#  Univariate 2021
 
 
 ## select the variables needed for analysis
@@ -211,8 +206,8 @@ BMI_complete_categories_2021 <- BMI_complete_categories_2021 %>%
   dplyr::mutate(age_group = fct_relevel(age_group, "18-39", after = 0)) %>%
   dplyr::mutate(ethnic_no_miss = as.factor(ethnic_no_miss)) %>%#
   dplyr::mutate(ethnic_no_miss = fct_relevel(ethnic_no_miss, "White", after = 0)) %>%
-  dplyr::mutate(eth_group_16 = as.factor(ethnic_no_miss)) %>%#
-  dplyr::mutate(eth_group_16 = fct_relevel(ethnic_no_miss, "White", after = 0))
+  dplyr::mutate(eth_group_16 = as.factor(eth_group_16)) %>%#
+  dplyr::mutate(eth_group_16 = fct_relevel(eth_group_16, "White", after = 0))
 
 ## instructions from R for epi
 # Univariate model with age
@@ -300,9 +295,81 @@ univariate_had_bmi_2021 <- univ_tab_base  %>%
   dplyr::mutate(proportion = had_bmi/N_total, .after=had_bmi) %>%
   mutate(across(where(is.numeric), round, digits = 2))
 
+#####################################################################################################################
+#####################################################################################################################
+
+
+#####################################################################################################
+# 2.  Multivariate 2020
+
+
+
+# create a new vector of explanatory variables - exclude age, gender, ethnicity
+
+explanatory_vars_multivariate <- c("region", 
+                                   "imd",
+                                   "precovid_obese_flag", 
+                                   "comorbid_learning_disability",     
+                                   "comorbid_depression",            
+                                   "comorbid_dementia",               
+                                   "comorbid_psychosis_schiz_bipolar",
+                                   "comorbid_diabetes_type",
+                                   "comorbid_diabetes_t1",           
+                                   "comorbid_diabetes_t2",             
+                                   "comorbid_asthma",                
+                                   "comorbid_COPD",                   
+                                   "comorbid_stroke_and_TIA",         
+                                   "comorbid_chronic_cardiac", 
+                                   "comorbid_hypertension",           
+                                   "comorbid_all_cancer")
+
+
+# >> Example model
+# Multivariate model with age +gender
+had_bmi_age_sex_m <- glm(had_bmi ~ age_group + sex, data=BMI_complete_categories_2020, family=binomial) %>%
+  broom::tidy(exponentiate = TRUE, conf.int = TRUE) %>%        # exponentiate and produce CIs
+  dplyr::mutate(across(where(is.numeric), round, digits = 2))  # round all numeric columns
+
+
+
+#>> Use PURR to loop over the different exposures in a univariate analysis and create a combined table
+
+#>>  use stringer to create a vector listing each item to run the logistic regression over
+models_age_sex_ethnic_2020 <- explanatory_vars_multivariate %>%       # begin with variables of interest
+  str_c("had_bmi ~ age_group + sex + eth_group_16 + ", .)  %>%      ## creates a vector of character with terms for age, gender and ethnicity regression
+  
+  # iterate through each univariate formula ... using map function from purr
+  map(                               #  Map each element of the preceding vector the following formula
+    .f = ~glm(                       # pass the formulas one-by-one to glm()
+      formula = as.formula(.x),      # within glm(), the string formula is .x
+      family = "binomial",           # specify type of glm (logistic)
+      data = BMI_complete_categories_2020))  %>%        # dataset
+  
+  # tidy up each of the glm regression outputs from above
+  map(
+    .f = ~tidy(
+      .x, 
+      exponentiate = TRUE,           # exponentiate 
+      
+      
+      conf.int = TRUE)) %>%          # return confidence intervals
+  
+  # collapse the list of regression outputs in to one data frame
+  bind_rows() %>% 
+  
+  # round all numeric columns
+  mutate(across(where(is.numeric), round, digits = 2))
+
+
+
+
+
 
 
 ####################################################################################
 ## OUTPUTS
 write.csv (univariate_had_bmi_2020, here::here ("output/data","regression_had_bmi_2020.csv"))
 write.csv (univariate_had_bmi_2021, here::here ("output/data","regression_had_bmi_2021.csv"))
+write.csv (models_age_sex_ethnic_2020, here::here ("output/data","multivariate_regression_had_bmi_2020.csv"))
+
+
