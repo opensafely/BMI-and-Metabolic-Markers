a10640616b07e29113f7c5989e3252d8ab008ea3
Miriam-S-git <74236842+Miriam-S-git@users.noreply.github.com>
2022-03-14 11:58:03 +0000

    Update had_bmi_regression.R

--- analysis/had_bmi_regression.R
+++ analysis/had_bmi_regression.R
@@ -326,9 +326,9 @@ explanatory_vars_multivariate <- c("region",
 
 # >> Example model
 # Multivariate model with age +gender
-had_bmi_age_sex_m <- glm(had_bmi ~ age_group + sex, data=BMI_complete_categories_2020, family=binomial) %>%
-  broom::tidy(exponentiate = TRUE, conf.int = TRUE) %>%        # exponentiate and produce CIs
-  dplyr::mutate(across(where(is.numeric), round, digits = 2))  # round all numeric columns
+# had_bmi_age_sex_m <- glm(had_bmi ~ age_group + sex, data=BMI_complete_categories_2020, family=binomial) %>%
+  # broom::tidy(exponentiate = TRUE, conf.int = TRUE) %>%        # exponentiate and produce CIs
+  # dplyr::mutate(across(where(is.numeric), round, digits = 2))  # round all numeric columns
 
 
 
@@ -361,6 +361,70 @@ models_age_sex_ethnic_2020 <- explanatory_vars_multivariate %>%       # begin wi
   mutate(across(where(is.numeric), round, digits = 2))
 
 
+##########################################################################################################
+
+#####################################################################################################
+# 2.  Multivariate 2021
+
+
+
+# create a new vector of explanatory variables - exclude age, gender, ethnicity
+
+explanatory_vars_multivariate <- c("region", 
+                                   "imd",
+                                   "precovid_obese_flag", 
+                                   "comorbid_learning_disability",     
+                                   "comorbid_depression",            
+                                   "comorbid_dementia",               
+                                   "comorbid_psychosis_schiz_bipolar",
+                                   "comorbid_diabetes_type",
+                                   "comorbid_diabetes_t1",           
+                                   "comorbid_diabetes_t2",             
+                                   "comorbid_asthma",                
+                                   "comorbid_COPD",                   
+                                   "comorbid_stroke_and_TIA",         
+                                   "comorbid_chronic_cardiac", 
+                                   "comorbid_hypertension",           
+                                   "comorbid_all_cancer")
+
+
+# >> Example model
+# Multivariate model with age +gender
+# had_bmi_age_sex_m <- glm(had_bmi ~ age_group + sex, data=BMI_complete_categories_2021, family=binomial) %>%
+  # broom::tidy(exponentiate = TRUE, conf.int = TRUE) %>%        # exponentiate and produce CIs
+  # dplyr::mutate(across(where(is.numeric), round, digits = 2))  # round all numeric columns
+
+
+
+#>> Use PURR to loop over the different exposures in a univariate analysis and create a combined table
+
+#>>  use stringer to create a vector listing each item to run the logistic regression over
+models_age_sex_ethnic_2021 <- explanatory_vars_multivariate %>%       # begin with variables of interest
+  str_c("had_bmi ~ age_group + sex + eth_group_16 + ", .)  %>%      ## creates a vector of character with terms for age, gender and ethnicity regression
+  
+  # iterate through each univariate formula ... using map function from purr
+  map(                               #  Map each element of the preceding vector the following formula
+    .f = ~glm(                       # pass the formulas one-by-one to glm()
+      formula = as.formula(.x),      # within glm(), the string formula is .x
+      family = "binomial",           # specify type of glm (logistic)
+      data = BMI_complete_categories_2021))  %>%        # dataset
+  
+  # tidy up each of the glm regression outputs from above
+  map(
+    .f = ~tidy(
+      .x, 
+      exponentiate = TRUE,           # exponentiate 
+      
+      
+      conf.int = TRUE)) %>%          # return confidence intervals
+  
+  # collapse the list of regression outputs in to one data frame
+  bind_rows() %>% 
+  
+  # round all numeric columns
+  mutate(across(where(is.numeric), round, digits = 2))
+
+
 
 
 
@@ -371,5 +435,5 @@ models_age_sex_ethnic_2020 <- explanatory_vars_multivariate %>%       # begin wi
 write.csv (univariate_had_bmi_2020, here::here ("output/data","regression_had_bmi_2020.csv"))
 write.csv (univariate_had_bmi_2021, here::here ("output/data","regression_had_bmi_2021.csv"))
 write.csv (models_age_sex_ethnic_2020, here::here ("output/data","multivariate_regression_had_bmi_2020.csv"))
-
+write.csv (models_age_sex_ethnic_2021, here::here ("output/data","multivariate_regression_had_bmi_2021.csv"))
 
