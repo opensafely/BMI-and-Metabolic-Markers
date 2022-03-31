9b5273dba23d8313bf93bac3a01423a4a37d2431
Miriam-S-git <74236842+Miriam-S-git@users.noreply.github.com>
2022-03-31 06:57:02 +0100

    Update common_variables.py

--- analysis/common_variables.py
+++ analysis/common_variables.py
@@ -228,7 +228,7 @@ bmi=patients.most_recent_bmi(
     return_expectations={
         "date": {"earliest": "2010-02-01", "latest": "2022-02-01"},
         "float": {"distribution": "normal", "mean": 28, "stddev": 8},
-        "incidence": 0.10,
+        "incidence": 0.7,
     }
     
     ),
@@ -258,7 +258,7 @@ bmi_march=patients.most_recent_bmi(
     return_expectations={
         "date": {"earliest": "2015-03-01", "latest": "2015-04-01"},
         "float": {"distribution": "normal", "mean": 28, "stddev": 8},
-        "incidence": 0.01,
+        "incidence": 0.1,
     }
     
     ),
@@ -272,7 +272,7 @@ bmi_march=patients.most_recent_bmi(
     return_expectations={
         "date": {"earliest": "2015-04-01", "latest": "2015-05-01"},
         "float": {"distribution": "normal", "mean": 28, "stddev": 8},
-        "incidence": 0.01,
+        "incidence": 0.05,
     }
     
     ),   
@@ -285,7 +285,7 @@ bmi_march=patients.most_recent_bmi(
     return_expectations={
         "date": {"earliest": "2015-05-01", "latest": "2015-06-01"},
         "float": {"distribution": "normal", "mean": 28, "stddev": 8},
-        "incidence": 0.01,
+        "incidence": 0.1,
     }
     
     ),   
@@ -299,7 +299,7 @@ bmi_march=patients.most_recent_bmi(
     return_expectations={
         "date": {"earliest": "2015-06-01", "latest": "2015-07-01"},
         "float": {"distribution": "normal", "mean": 28, "stddev": 8},
-        "incidence": 0.01,
+        "incidence": 0.05,
     }
     
     ),   
@@ -313,7 +313,7 @@ bmi_march=patients.most_recent_bmi(
     return_expectations={
         "date": {"earliest": "2015-07-01", "latest": "2015-08-01"},
         "float": {"distribution": "normal", "mean": 28, "stddev": 8},
-        "incidence": 0.01,
+        "incidence": 0.1,
     }
     
     ),   
@@ -327,7 +327,7 @@ bmi_march=patients.most_recent_bmi(
     return_expectations={
         "date": {"earliest": "2015-08-01", "latest": "2015-09-01"},
         "float": {"distribution": "normal", "mean": 28, "stddev": 8},
-        "incidence": 0.01,
+        "incidence": 0.05,
     }
     
     ),   
@@ -342,7 +342,7 @@ bmi_march=patients.most_recent_bmi(
     return_expectations={
         "date": {"earliest": "2015-09-01", "latest": "2015-10-01"},
         "float": {"distribution": "normal", "mean": 28, "stddev": 8},
-        "incidence": 0.01,
+        "incidence": 0.1,
     }
     
     ),   
@@ -356,7 +356,7 @@ bmi_march=patients.most_recent_bmi(
     return_expectations={
         "date": {"earliest": "2015-10-01", "latest": "2015-11-01"},
         "float": {"distribution": "normal", "mean": 28, "stddev": 8},
-        "incidence": 0.01,
+        "incidence": 0.05,
     }
     
     ),   
@@ -370,7 +370,7 @@ bmi_march=patients.most_recent_bmi(
     return_expectations={
         "date": {"earliest": "2015-11-01", "latest": "2015-12-01"},
         "float": {"distribution": "normal", "mean": 28, "stddev": 8},
-        "incidence": 0.01,
+        "incidence": 0.05,
     }
     
     ),   
@@ -385,7 +385,7 @@ bmi_march=patients.most_recent_bmi(
     return_expectations={
         "date": {"earliest": "2015-12-01", "latest": "2016-01-01"},
         "float": {"distribution": "normal", "mean": 28, "stddev": 8},
-        "incidence": 0.01,
+        "incidence": 0.05,
     }
     
     ),   
@@ -399,7 +399,7 @@ bmi_march=patients.most_recent_bmi(
     return_expectations={
         "date": {"earliest": "2016-01-01", "latest": "2016-02-01"},
         "float": {"distribution": "normal", "mean": 28, "stddev": 8},
-        "incidence": 0.01,
+        "incidence": 0.05,
     }
     
     ),   
