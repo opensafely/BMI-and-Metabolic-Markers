67bea3dde64acf64362aa84fd3a40a6166aa2ef0
Miriam-S-git <miriam.samuel@nhs.net>
2022-04-08 07:12:21 +0000

    check variables extracted

--- analysis/check_hba1c.R
+++ analysis/check_hba1c.R
@@ -11,12 +11,88 @@ library(forcats)
 library(rstatix)
 library(janitor)
 
-check_hba1c <- read_feather (here::here ("output/data", "input_all_2018-03-01.feather"))
+all_2015 <- read_feather (here::here ("output/data", "input_all_2015-03-01.feather"))
 
-check_hba1c <- check_hba1c %>%
+check_age_group <- all_2015 %>%
+  tabyl(age_group)  %>%
+  dplyr:: rename(check = 'percent') %>%
+  dplyr::rename(group = 'age_group') %>%
+  dplyr::mutate(variable = 'age_group') %>%
+  dplyr::select('group', 'check', 'variable') %>%
+  dplyr::mutate(check = as.character(check))
+
+check_age_group_2 <- all_2015 %>%
+  tabyl(age_group_2)  %>%
+  dplyr:: rename(check = 'percent') %>%
+  dplyr::rename(group = 'age_group_2') %>%
+  dplyr::mutate(variable = 'age_group_2') %>%
+  dplyr::select('group', 'check', 'variable') %>%
+  dplyr::mutate(check = as.character(check))
+
+
+
+check_hba1c <- all_2015 %>%
   ungroup() %>%
   dplyr::filter(hba1c_march >0 ) %>%
   dplyr::group_by(sex) %>%
-  dplyr::summarise(mean_hba1c_march = mean(hba1c_march))
+  dplyr::rename(group = 'sex') %>%
+  dplyr::summarise(check = mean(hba1c_march)) %>%
+  dplyr::mutate(check = as.character(check)) %>%
+  dplyr::mutate(variable = 'hba1c')
+
+
+
+ check_bmi <- all_2015 %>%
+  ungroup() %>%
+  dplyr::filter(bmi_march >0 ) %>%
+  dplyr::group_by(sex) %>%
+   dplyr::rename(group = 'sex') %>%
+  dplyr::summarise(check = mean(bmi_march)) %>%
+   dplyr::mutate(check = as.character(check)) %>%
+   dplyr::mutate(variable = 'bmi')
+ 
+ 
+ check_chol <- all_2015 %>%
+   tabyl(sex, cholesterol_test) %>%
+   adorn_percentages("row") %>%
+   adorn_pct_formatting(digits = 2) %>%
+   adorn_ns() %>%
+   dplyr:: rename(check = 'TRUE') %>%
+   dplyr::select(sex, check) %>%
+   dplyr::rename(group = 'sex') %>%
+   dplyr::mutate(variable = 'chol')
+
+
+
+ 
+
+ 
+check_sbp <- all_2015 %>%
+  filter(sbp>0) %>%
+  group_by(sex) %>%
+  dplyr::rename(group = 'sex') %>%
+  summarise_at(vars(sbp), list(check=mean)) %>%
+  dplyr::mutate(check = as.character(check)) %>%
+  dplyr::mutate(variable = 'sbp')
+ 
+ 
+check_dbp <- all_2015 %>%
+  filter(dbp>0) %>%
+  group_by(sex) %>%
+  dplyr::rename(group = 'sex') %>%
+  summarise_at(vars(dbp), list(check = mean)) %>%
+  dplyr::mutate(check = as.character(check)) %>%
+  dplyr::mutate(variable = 'dbp')
+
+check_hba1c <- check_hba1c %>%
+  bind_rows(check_bmi) %>%
+  bind_rows(check_chol) %>%
+  bind_rows(check_sbp) %>%
+  bind_rows(check_dbp) %>%
+  bind_rows(check_chol) %>%
+  bind_rows(check_age_group) %>%
+  bind_rows(check_age_group_2)
+
+check_hba1c
 
 write.csv (check_hba1c, here::here ("output/data","check_hba1c.csv"))
