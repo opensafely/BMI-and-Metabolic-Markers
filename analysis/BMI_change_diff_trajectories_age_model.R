f2205e40be32efbbc9012caf2b4c7e3d38a406ea
Miriam-S-git <miriam.samuel@nhs.net>
2022-05-25 14:01:28 +0000

    age adjusted trajectory change by DWMP groups

--- analysis/BMI_change_diff_trajectories_age_model.R
+++ analysis/BMI_change_diff_trajectories_age_model.R
@@ -1,6 +1,6 @@
-## This script will develop linear regression models of predictors of change in BMI trajectory adjusted for age
+## This script will develop linear regression models of predictors of change in BMI trajectory
 ## Author: Miriam Samuel 
-## Date: 17th May
+## Date: 5th May
 
 
 ## Load libraries
@@ -18,6 +18,7 @@ library(skimr)
 library(ggplot2)
 
 
+
 BMI_DT <- read_feather (here::here ("output/data", "BMI_trajectory_models_data.feather"))
 
 
@@ -29,7 +30,6 @@ BMI_DT <- read_feather (here::here ("output/data", "BMI_trajectory_models_data.f
 
 # develop a vector of explanatory variables
 explanatory_vars <- c("sex", 
-                      "age_group_2", 
                       "precovid_bmi_category", 
                       "ethnic_no_miss", 
                       "eth_group_16",
@@ -54,7 +54,7 @@ explanatory_vars <- c("sex",
 models_trajectory_change <- explanatory_vars %>%       # begin with variables of interest
   str_c("trajectory_change ~ age_group_2 +", .) %>%         # combine each variable into formula ("outcome ~ variable of interest")
   
-  # iterate through each univariate formula
+  # iterate through each age_adjusted formula
   map(                               
     .f = ~lm(                       # pass the formulas one-by-one to glm()
       formula = as.formula(.x),      # within glm(), the string formula is .x
@@ -71,15 +71,169 @@ models_trajectory_change <- explanatory_vars %>%       # begin with variables of
   bind_rows() %>% 
   
   # round all numeric columns
-  mutate(across(where(is.numeric), round, digits = 2))
+  mutate(across(where(is.numeric), round, digits = 4))
 
 
 
 
 models_trajectory_change
 
+## Limit to patients with hypertension
+BMI_DT_hypertension <-  BMI_DT %>% 
+dplyr::filter(hypertension == TRUE)
 
 
+
+
+## LINEAR REGRESSION MODELLING
+### Predictors of trajectory change
+
+
+# develop a vector of explanatory variables
+explanatory_vars_hypertension <- c("sex", 
+                      "precovid_bmi_category", 
+                      "ethnic_no_miss", 
+                      "eth_group_16",
+                      "imd", 
+                      "region", 
+                      "diabetes_t1",           
+                      "diabetes_t2",
+                      "chronic_cardiac", 
+                      "learning_disability",     
+                      "depression",            
+                      "dementia",               
+                      "psychosis_schiz_bipolar",
+                      "asthma",                
+                      "COPD",                   
+                      "stroke_and_TIA",       
+                      "all_cancer", 
+                      "smoking_status")
+
+
+
+models_trajectory_change_hypertension <- explanatory_vars_hypertension %>%       # begin with variables of interest
+  str_c("trajectory_change ~ age_group_2 +", .) %>%         # combine each variable into formula ("outcome ~ variable of interest")
+  
+  # iterate through each age_adjusted formula
+  map(                               
+    .f = ~lm(                       # pass the formulas one-by-one to glm()
+      formula = as.formula(.x),      # within glm(), the string formula is .x
+      data = BMI_DT_hypertension)) %>%          # dataset
+  
+  # tidy up each of the glm regression outputs from above
+  map(
+    .f = ~tidy(
+      .x, 
+      exponentiate = TRUE,           # exponentiate 
+      conf.int = TRUE)) %>%          # return confidence intervals
+  
+  # collapse the list of regression outputs in to one data frame
+  bind_rows() %>% 
+  
+  # round all numeric columns
+  mutate(across(where(is.numeric), round, digits = 4))
+
+
+
+
+models_trajectory_change_hypertension
+
+
+## Limit to patients with T2 diabetes
+BMI_DT_T2DM <-  BMI_DT %>% 
+dplyr::filter(diabetes_t2 == TRUE)
+
+
+
+
+## LINEAR REGRESSION MODELLING
+### Predictors of trajectory change
+
+
+# develop a vector of explanatory variables
+explanatory_vars_diabetes <- c("sex", 
+                      "precovid_bmi_category", 
+                      "ethnic_no_miss", 
+                      "eth_group_16",
+                      "imd", 
+                      "region", 
+                      "hypertension",           
+                      "chronic_cardiac", 
+                      "learning_disability",     
+                      "depression",            
+                      "dementia",               
+                      "psychosis_schiz_bipolar",
+                      "asthma",                
+                      "COPD",                   
+                      "stroke_and_TIA",       
+                      "all_cancer", 
+                      "smoking_status")
+
+
+
+models_trajectory_change_T2DM <- explanatory_vars_diabetes %>%       # begin with variables of interest
+  str_c("trajectory_change ~ age_group_2 +", .) %>%         # combine each variable into formula ("outcome ~ variable of interest")
+  
+  # iterate through each age_adjusted formula
+  map(                               
+    .f = ~lm(                       # pass the formulas one-by-one to glm()
+      formula = as.formula(.x),      # within glm(), the string formula is .x
+      data = BMI_DT_T2DM)) %>%          # dataset
+  
+  # tidy up each of the glm regression outputs from above
+  map(
+    .f = ~tidy(
+      .x, 
+      exponentiate = TRUE,           # exponentiate 
+      conf.int = TRUE)) %>%          # return confidence intervals
+  
+  # collapse the list of regression outputs in to one data frame
+  bind_rows() %>% 
+  
+  # round all numeric columns
+  mutate(across(where(is.numeric), round, digits = 4))
+
+
+
+
+models_trajectory_change_T2DM
+
+BMI_DT_T1DM <-  BMI_DT %>% 
+dplyr::filter(diabetes_t1 == TRUE)
+
+
+
+
+## LINEAR REGRESSION MODELLING
+### Predictors of trajectory change
+
+
+models_trajectory_change_T1DM <- explanatory_vars_diabetes %>%       # begin with variables of interest
+  str_c("trajectory_change ~ age_group_2 +", .) %>%         # combine each variable into formula ("outcome ~ variable of interest")
+  
+  # iterate through each age_adjusted formula
+  map(                               
+    .f = ~lm(                       # pass the formulas one-by-one to glm()
+      formula = as.formula(.x),      # within glm(), the string formula is .x
+      data = BMI_DT_T1DM)) %>%          # dataset
+  
+  # tidy up each of the glm regression outputs from above
+  map(
+    .f = ~tidy(
+      .x, 
+      exponentiate = TRUE,           # exponentiate 
+      conf.int = TRUE)) %>%          # return confidence intervals
+  
+  # collapse the list of regression outputs in to one data frame
+  bind_rows() %>% 
+  
+  # round all numeric columns
+  mutate(across(where(is.numeric), round, digits = 4))
+
+
+
+
+models_trajectory_change_T1DM
 
 
 
@@ -89,4 +243,13 @@ models_trajectory_change
 
 
 write.csv (models_trajectory_change, here::here ("output/data","age_adjusted_bmi_trajectory_change.csv"))
+write.csv (models_trajectory_change_hypertension, here::here ("output/data","age_adjusted_bmi_trajectory_change_hypertension.csv"))
+write.csv (models_trajectory_change_T2DM, here::here ("output/data","age_adjusted_bmi_trajectory_change_T2DM.csv"))
+write.csv (models_trajectory_change_T1DM, here::here ("output/data","age_adjusted_bmi_trajectory_change_T1DM.csv"))
+
+
+
+
+
+
 
