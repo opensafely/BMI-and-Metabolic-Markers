953a4dbb1b1baf7ba7d665643823134834a0953b
Miriam-S-git <miriam.samuel@nhs.net>
2022-06-23 12:36:48 +0000

    MICE data prep

---
+++ analysis/sensitivity_analyses/MICE_data_prep2.R
@@ -0,0 +1,70 @@
+##  This R script develops the imputed data frame
+
+## Specify libraries
+library(pacman)
+library(tidyverse)
+library(Hmisc)
+library(here)
+library(arrow)
+library(purrr)
+library(broom)
+library(data.table)
+library(forcats)
+library(rstatix)
+library(janitor)
+library(lubridate)
+library(skimr)
+library(ggplot2)
+library(mice)
+
+
+BMI_trajectories <- read_csv (here::here ("output/data", "imputation_data_long.csv"))
+
+## Sample
+BMI_trajectories <- BMI_trajectories[sample(nrow(BMI_trajectories), 2500), ]
+
+
+
+BMI_trajectories$imd <- factor(BMI_trajectories$imd, 
+                                levels = c('1','2','3','4','5'))
+
+
+
+# Unordered categorical variable 
+poly2 <- c("sex", "age_group_2", "region", "imd", "eth_group_16",  "smoking_status")
+
+
+
+
+
+
+# We run the mice code with 0 iterations 
+
+imp <- mice(BMI_trajectories, maxit=0)
+
+# Extract predictorMatrix and methods of imputation 
+
+predM <- imp$predictorMatrix
+meth <- imp$method
+
+meth
+
+## methods used for imputation are appropriate, don't need to change
+meth[poly2] <- "polyreg"
+
+meth
+
+## complete the imputation
+# With this command, we tell mice to impute the anesimp2 data, create 5
+# datasets, use predM as the predictor matrix and don't print the imputation
+# process. If you would like to see the process, set print as TRUE
+
+imp2 <- mice(BMI_trajectories, maxit = 5, 
+             predictorMatrix = predM, 
+             method = meth, print =  TRUE)
+
+
+
+BMI_imp_long <- mice::complete(imp2, action="long", include = TRUE)
+
+write.csv (BMI_imp_long, here::here ("output/data", "imputation_dataframe.csv"))
