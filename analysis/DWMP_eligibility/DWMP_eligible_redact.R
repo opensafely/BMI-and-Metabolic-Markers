31d1038159846feb63ef7c1fecf7bebff0c18cb2
Miriam-S-git <m.samuel@qmul.ac.uk>
2022-07-28 13:36:26 +0000

    update redace

--- analysis/DWMP_eligibility/DWMP_eligible_redact.R
+++ analysis/DWMP_eligibility/DWMP_eligible_redact.R
@@ -34,6 +34,10 @@ d_all$eligible[d_all$eligible == 5] <- "6-10"
 d_all$no_bmi[d_all$no_bmi == 5] <- "6-10"
 d_all$not_eligible[d_all$not_eligible == 5] <- "6-10"
 
+
+d_all<-d_all[!(d_all$group=="African"),]
+
+
 d_T1DM$eligible[d_T1DM$eligible == 5] <- "6-10"
 d_T1DM$no_bmi[d_T1DM$no_bmi == 5] <- "6-10"
 d_T1DM$not_eligible[d_T1DM$not_eligible == 5] <- "6-10"
@@ -45,6 +49,8 @@ d_T2DM$eligible[d_T2DM$eligible == 5] <- "6-10"
 d_T2DM$no_bmi[d_T2DM$no_bmi == 5] <- "6-10"
 d_T2DM$not_eligible[d_T2DM$not_eligible == 5] <- "6-10"
 
+d_T2DM<-d_T2DM[!(d_T2DM$group=="African"),]
+
 
 
 
@@ -52,7 +58,7 @@ d_hypertension$eligible[d_hypertension$eligible == 5] <- "6-10"
 d_hypertension$no_bmi[d_hypertension$no_bmi == 5] <- "6-10"
 d_hypertension$not_eligible[d_hypertension$not_eligible == 5] <- "6-10"
 
-
+d_hypertension<-d_hypertension[!(d_hypertension$group=="African"),]
 
 
 write.csv (d_T1DM, here::here ("output/data", "DWMP_eligible_5yr_data_T1DM_redact.csv"))
