d95e99c8ec119980a1a7d2034809b09c4a7da4d4
Miriam-S-git <m.samuel@qmul.ac.uk>
2022-11-11 18:39:47 +0000

    ethnicity checks

--- analysis/ethnicity_check/check_ethnicity.R
+++ analysis/ethnicity_check/check_ethnicity.R
@@ -21,7 +21,7 @@ BMI_data <- read_feather (here::here ("output/data", "input_all_2021-03-01.feath
 
 meds_data <- read_feather (here::here ("output/data", "complete_meds_2021.feather"))
 
-
+median_data <- read_feather (here::here ("output/data", "BMI_complete_median_2021.feather"))
 
 colnames(BMI_data)
 
@@ -35,11 +35,15 @@ BMI_data %>%
 
 eth_16 <- BMI_data %>% 
     janitor::tabyl(ethnicity_16) %>% 
-    dplyr::mutate (group = "eth_16")
+    dplyr::mutate (group = "eth_16") %>% 
+    dplyr::mutate (data = "all_data") 
 
 eth_6 <- BMI_data %>% 
  janitor::tabyl(ethnicity) %>%
- dplyr::mutate (group = "eth_6")
+ dplyr::mutate (group = "eth_6") %>% 
+    dplyr::mutate (data = "all_data") 
+
+print("meds")
 
 meds_data %>% 
     janitor::tabyl(ethnicity_16) %>% 
@@ -51,19 +55,45 @@ meds_data %>%
 
 meds_eth_16 <- meds_data %>% 
     janitor::tabyl(ethnicity_16) %>% 
-    dplyr::mutate (meds = "meds_join")
+    dplyr::mutate (group = "eth_16")%>% 
+    dplyr::mutate (data = "meds_join")
 
 meds_eth_6 <- meds_data %>% 
  janitor::tabyl(ethnicity) %>%
  dplyr::mutate (group = "eth_6")%>% 
- dplyr::mutate (meds = "meds_join") 
+ dplyr::mutate (data = "meds_join") 
+
+print("median")
+
+median_data %>% 
+ janitor::tabyl(ethnic_no_miss) %>%
+ dplyr::mutate (group = "eth_6")%>% 
+ dplyr::mutate (data = "median") 
 
 
+median_data %>% 
+ janitor::tabyl(eth_group_16) %>%
+ dplyr::mutate (group = "eth_16")%>% 
+ dplyr::mutate (data = "median")
 
- eth_16 <- bind_rows(eth_6, eth_16, meds_eth_6, meds_eth_16)
+median_eth_6 <- median_data %>% 
+ janitor::tabyl(ethnic_no_miss) %>%
+ dplyr::mutate (group = "eth_6")%>% 
+ dplyr::mutate (data = "median") 
 
- eth_16 <-  eth_16 %>%
- dplyr::select(meds, group, ethnicity, ethnicity_16, n, percent)
+
+median_eth_16 <- median_data %>% 
+ janitor::tabyl(eth_group_16) %>%
+ dplyr::mutate (group = "eth_16")%>% 
+ dplyr::mutate (data = "median") 
+
+
+ eth_16 <- bind_rows(eth_6, eth_16, meds_eth_6, meds_eth_16, median_eth_6, median_eth_16)
+
+eth_16
+
+eth_16 <-  eth_16 %>%
+ dplyr::select(data, group, ethnicity, ethnicity_16, n, percent, ethnic_no_miss, eth_group_16)
 
 
 
