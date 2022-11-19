c885e51e4618ec439afbaae6188ecfa0075610cf
Miriam-S-git <m.samuel@qmul.ac.uk>
2022-11-19 15:38:32 +0000

    split ethnicity corrected eth adjusted

---
+++ analysis/ethnicity_check/eth_corrected_regression/rapid_change_ethnicity_adjusted_precov.R
@@ -0,0 +1,179 @@
+
+## M Samuel 
+## 5th May 2022
+## This script checks factors associated with rapid weight gain if patients with cancer and a low BMI prepandemic are removed
+
+
+## Load libraries
+## Specify libraries
+library(pacman)
+library(tidyverse)
+library(Hmisc)
+library(here)
+library(arrow)
+library(purrr)
+library(broom)
+library(data.table)
+library(forcats)
+library(rstatix)
+library(janitor)
+library(lubridate)
+library(skimr)
+library(ggplot2)
+library(gtsummary)
+
+
+
+
+
+## Read in files
+BMI_2 <- read_feather (here::here ("output/data", "BMI_trajectory_data_long_eth_corrected.feather"))
+
+
+colnames(BMI_2)
+
+BMI_2 %>% 
+  tabyl(eth_16_corrected)
+
+
+
+
+BMI_trajectories <- BMI_2 %>% 
+  dplyr::select("sex",
+                "age_group_2", 
+                "region",                
+                "imd",
+                "hypertension",
+                "diabetes_t1", 
+                "diabetes_t2",
+                "learning_disability", 
+                "depression",               
+                "psychosis_schiz_bipolar", 
+                "dementia", 
+                "asthma",
+                "COPD",
+                "stroke_and_TIA",          
+                "chronic_cardiac", 
+                "all_cancer",                
+                "smoking_status", 
+                "eth_16_corrected",
+                "complete_bmi_data", 
+                "bmi_change_cat", 
+                "precovid_bmi_category", 
+                "pandemic_stage")
+
+
+BMI_trajectories <- BMI_trajectories %>%
+  dplyr::mutate(eth_16_corrected = factor(eth_16_corrected, 
+                                          levels = c("White_British",
+                                                     "White_Irish",
+                                                     "Other_White",
+                                                     "White_Black_Carib",
+                                                     "White_Black_African",
+                                                     "White_Asian",
+                                                     "Other_Mixed",
+                                                     "Indian",
+                                                     "Pakistani",
+                                                     "Bangladeshi",
+                                                     "Other_Asian",
+                                                     "Chinese",
+                                                     "Caribbean",
+                                                     "African",
+                                                     "Other_Black",
+                                                     "Other")) )
+
+
+
+BMI_trajectories <- BMI_trajectories %>% 
+  dplyr::mutate(rapid_bmi_change = case_when(
+    bmi_change_cat == 'over 0.5' ~ 1, 
+    bmi_change_cat != 'over 0.5' ~ 0, 
+  ))
+
+BMI_trajectories$precovid_bmi_category <- factor(BMI_trajectories$precovid_bmi_category, levels = c("healthy","overweight", "obese", "underweight"))
+
+
+
+
+explanatory_vars<- c( "region",                
+                      "hypertension",
+                      "diabetes_t1", 
+                      "diabetes_t2",
+                      "learning_disability", 
+                      "depression",               
+                      "psychosis_schiz_bipolar", 
+                      "dementia", 
+                      "asthma",
+                      "COPD",
+                      "stroke_and_TIA",          
+                      "chronic_cardiac",              
+                      "smoking_status", 
+                      "eth_16_corrected",           
+                      "precovid_bmi_category")
+
+
+
+
+
+##*** Change to code.  FILTER OUT UNDERWEIGHT AND THOSE WITH CANCER
+BMI_trajectories <- BMI_trajectories %>% 
+  dplyr::filter(all_cancer == FALSE)
+
+BMI_trajectories <- BMI_trajectories %>% 
+  dplyr::filter(precovid_bmi_category != "underweight")
+
+BMI_trajectories %>% 
+  tabyl(all_cancer)
+
+BMI_trajectories %>% 
+  tabyl(precovid_bmi_category)
+
+## *** Change to code complete
+
+
+
+
+
+
+
+## Precovid analysis proportions in groups
+
+
+### Precovid analysis
+
+
+precovid_change <- BMI_trajectories %>% 
+  dplyr::filter(pandemic_stage == "precovid")
+
+# age adjusted
+models_precov <- explanatory_vars %>%       # begin with variables of interest
+  str_c("rapid_bmi_change ~ eth_16_corrected + ", .) %>%         # combine each variable into formula ("outcome ~ variable of interest")
+  
+  # iterate through each univariate formula
+  map(                               
+    .f = ~glm(                       # pass the formulas one-by-one to glm()
+      formula = as.formula(.x),      # within glm(), the string formula is .x
+      family = "binomial",           # specify type of glm (logistic)
+      data = precovid_change)) %>%          # dataset
+  
+  # tidy up each of the glm regression outputs from above
+  map(
+    .f = ~tidy(
+      .x, 
+      exponentiate = TRUE,           # exponentiate 
+      conf.int = TRUE)) %>%          # return confidence intervals
+  
+  # collapse the list of regression outputs in to one data frame
+  bind_rows() %>% 
+  
+  # round all numeric columns
+  mutate(across(where(is.numeric), round, digits = 4)) 
+
+models_precov <- models_precov %>%
+  dplyr::mutate(stage = "precovid", .before = 1) 
+
+
+
+
+
+write_csv (models_precov, here::here ("output/data","rapid_bmi_change_cancerandlowbmi_removed_eth_corrected_eth_adjusted_precovid.csv"))
