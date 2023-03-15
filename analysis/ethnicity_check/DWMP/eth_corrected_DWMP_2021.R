922b967451a40c20df9094551c8c31b569e606d8
Miriam-S-git <m.samuel@qmul.ac.uk>
2023-03-15 12:53:08 +0000

    DWMP_2021

--- analysis/ethnicity_check/DWMP/eth_corrected_DWMP_2021.R
+++ analysis/ethnicity_check/DWMP/eth_corrected_DWMP_2021.R
@@ -1,11 +1,45 @@
+#### Author: M Samuel
+#### Date: March 2023
+#### This script calculates the proportion of the population eligible for the DWMP
 
-## This script will calculate the propotion eligible for DWMP
+## Specify libraries
+library(pacman)
+library(tidyverse)
+library(Hmisc)
+library(here)
+library(arrow)
+library(purrr)
+library(broom)
+library(data.table)
+library(forcats)
+library(rstatix)
+library(janitor)
+library(skimr)
 
-# 1.  load libraries
-# 2.  load the correct CSV file
 
 
-dt <- BMI_complete_median_eth_corrected
+
+
+
+##################################
+# READ IN FILES
+##################################
+
+dt <- read_csv(here::here("output/data", "BMI_complete_median_eth_corrected.csv"))
+
+age_group <- read_feather(here::here ("output/data", "complete_meds_2021.feather"))
+
+
+
+
+
+### create a joining file for age
+age_group <- age_group %>% 
+  dplyr::select("patient_id",
+                "age_group_2")
+
+
+
 
 
 
@@ -16,28 +50,30 @@ dt <- dt %>%
     "year",   
     "median_bmi", 
     "sex", 
-    "age_group_2",
     "region", 
     "imd", 
-    "smoking_status", 
-    "oad_meds", 
-    "insulin_meds", 
     "eth_16_corrected", 
     starts_with("comorbid")             
   )
 
+dt <- dt %>% 
+  left_join(age_group, by= 'patient_id')
 
-## Filter out years witha  missing BMI
+
+
+dt %>% 
+  tabyl(age_group_2)
+
+
+## Filter out years with  missing BMI
 dt <- dt %>% 
   drop_na(median_bmi)
 
 
-
-
 dt <- dt %>% 
   dplyr::mutate(minority = case_when(
-    (eth_16_corrected == "White_British" | eth_16_corrected == "White_Irish" | eth_16_corrected == "Other_White") ~ "white", 
-    eth_16_corrected == "None" ~ "None"
+    (eth_16_corrected == "White_British" | eth_16_corrected == "White_Irish" | eth_16_corrected == "Other_White"|     eth_16_corrected == "None" ) ~ "white", 
+
   )) 
 
  dt <- dt %>%  mutate(
@@ -49,8 +85,7 @@ dt <- dt %>%
    tabyl(minority)
  
 
-dt_2019 <- dt %>%
-  dplyr::filter(year <= 2019) 
+
 
 
 
@@ -58,18 +93,11 @@ dt_2021 <- dt %>%
   dplyr::filter(year >= 2017) 
 
 
-
-dt_2019 <- dt_2019 %>% 
-  dplyr::group_by(patient_id) %>% 
-  arrange(desc(year), .by_group=TRUE) %>% 
-  slice_head()
-
-
-
 dt_2021 <- dt_2021 %>% 
   dplyr::group_by(patient_id) %>% 
   arrange(desc(year), .by_group=TRUE)  %>% 
-  slice_head()
+  slice_head %>% 
+  ungroup()
 
 
 
@@ -96,26 +124,21 @@ dt_2021 <- dt_2021 %>%
   )) 
 
 
-###
-
-
-dt_2019 <- dt_2019 %>% 
-  dplyr::mutate(weight_universal = case_when(
-    median_bmi <18.5 ~ "underweight",
-    median_bmi >= 18.5 & median_bmi < 25 ~ "healthy",
-    median_bmi >= 25 & median_bmi < 30 ~ "overweight",
-    median_bmi >= 30 ~ "obese",
-  ))
-
-
-
- dt_2019 <- dt_2019 %>% 
-  dplyr::mutate(weight_DWMP = case_when(
-    ((median_bmi >= 30 & minority == "white") | (median_bmi >= 27.5 & minority == "not_white"))    ~  "DWMP_obese",
-    ((median_bmi < 30 & minority == "white") | (median_bmi < 27.5 & minority == "not_white"))    ~  "DWMP_not_obese"
-  )) 
-
-
+ 
+ 
+ dt_2021 <- dt_2021 %>% 
+   dplyr::mutate(dwmp_eligible = case_when(
+     (weight_DWMP == "DWMP_obese" & (comorbid_hypertension == "TRUE")) |(weight_DWMP == "DWMP_obese" & (comorbid_diabetes_t1 == "TRUE"))|(weight_DWMP == "DWMP_obese" & (comorbid_diabetes_t2 == "TRUE")) ~ "DWMP_eligible", 
+      (weight_DWMP == "DWMP_not_obese") ~ "not_dwmp", 
+     (weight_DWMP == "DWMP_obese" & ((comorbid_hypertension != "TRUE") & (comorbid_diabetes_t1 != "TRUE") & (comorbid_diabetes_t2 != "TRUE")))~ "not_dwmp"
+   ))
+ 
+ 
+dt_2021 %>% 
+  tabyl(dwmp_eligible)
+ 
+dt_2021 %>% 
+  tabyl(eth_16_corrected)
 
  ### WRITE FUNCTIONS
 
@@ -131,6 +154,14 @@ universal_weight_f <- function (data, var){
 DWMP_weight_f <- function (data, var){
   data %>% 
     tabyl({{ var }}, weight_DWMP) %>%
+    dplyr::rename(group = 1) %>% 
+    dplyr::mutate(group = as.factor(group))
+}
+
+
+DWMP_eligible_f <- function (data, var){
+  data %>% 
+    tabyl({{ var }}, dwmp_eligible) %>%
     dplyr::rename(group = 1) %>% 
     dplyr::mutate(group = as.factor(group))
 }
@@ -156,17 +187,10 @@ imd <- universal_weight_f(dt_2021,imd)%>%
 region <- universal_weight_f(dt_2021,region)%>%
   dplyr::mutate (variable = 'region', .before=1)
 
-
-
-#########################
-
-
 universal_weight <- sex %>% 
   bind_rows(age, eth, imd, region)
 
-
-
-
+####
 
 
 
@@ -194,8 +218,36 @@ DWMP_weight <- sex %>%
   bind_rows(age, eth, imd, region)
 
 
+
+
+### 
+
+sex <-  DWMP_eligible_f(dt_2021,sex) %>%
+  dplyr::mutate (variable = 'sex', .before=1)
+
+
+age <- DWMP_eligible_f(dt_2021,age_group_2)%>%
+  dplyr::mutate (variable = 'age', .before=1)
+
+eth <- DWMP_eligible_f(dt_2021,eth_16_corrected)%>%
+  dplyr::mutate (variable = 'eth', .before=1)
+
+imd <- DWMP_eligible_f(dt_2021,imd)%>%
+  dplyr::mutate (variable = 'imd', .before=1)
+
+
+region <- DWMP_eligible_f(dt_2021,region)%>%
+  dplyr::mutate (variable = 'region', .before=1)
+
+
+DWMP_eligible <- sex %>% 
+  bind_rows(age, eth, imd, region)
+
+
+
 summary_2021 <- universal_weight %>% 
-  left_join(DWMP_weight, by = c("variable", "group")) 
+  left_join(DWMP_weight, by = c("variable", "group"))%>% 
+  left_join(DWMP_eligible, by = c("variable", "group"))
 
 
 ### T2D 2021
@@ -284,11 +336,7 @@ universal_weight <- sex %>%
   bind_rows(age, eth, imd, region)
 
 
-
-
-
-
-
+###
 
 
 sex <-  DWMP_weight_f(T1D_2021,sex) %>%
@@ -321,7 +369,6 @@ T1D_2021 <- universal_weight %>%
 
 BP_2021 <- dt_2021 %>% 
   dplyr::filter(comorbid_hypertension == TRUE)
-
 
 
 
@@ -381,239 +428,28 @@ BP_2021 <- universal_weight %>%
 
 
 
+########### Join into single table
 
-############## 2019
+summary_2021 <- summary_2021 %>% 
+  dplyr::mutate (category = "all", .before=1)
 
 
+T1D_2021 <- T1D_2021 %>% 
+  dplyr::mutate (category = "T1D", .before=1)
 
-sex <-  universal_weight_f(dt_2019,sex) %>%
-  dplyr::mutate (variable = 'sex', .before=1)
 
+BP_2021 <- BP_2021 %>% 
+  dplyr::mutate (category = "BP", .before=1)
 
-age <- universal_weight_f(dt_2019,age_group_2)%>%
-  dplyr::mutate (variable = 'age', .before=1)
 
-eth <- universal_weight_f(dt_2019,eth_16_corrected)%>%
-  dplyr::mutate (variable = 'eth', .before=1)
+T2D_2021 <- T2D_2021 %>% 
+  dplyr::mutate (category = "T2D", .before=1)
 
-imd <- universal_weight_f(dt_2019,imd)%>%
-  dplyr::mutate (variable = 'imd', .before=1)
 
+summary_2021 <- summary_2021 %>% 
+  bind_rows(BP_2021, 
+            T2D_2021, 
+            T1D_2021)
 
-region <- universal_weight_f(dt_2019,region)%>%
-  dplyr::mutate (variable = 'region', .before=1)
 
-
-
-universal_weight <- sex %>% 
-  bind_rows(age, eth, imd, region)
-
-
-##
-
-sex <-  DWMP_weight_f(dt_2019,sex) %>%
-  dplyr::mutate (variable = 'sex', .before=1)
-
-
-age <- DWMP_weight_f(dt_2019,age_group_2)%>%
-  dplyr::mutate (variable = 'age', .before=1)
-
-eth <- DWMP_weight_f(dt_2019,eth_16_corrected)%>%
-  dplyr::mutate (variable = 'eth', .before=1)
-
-imd <- DWMP_weight_f(dt_2019,imd)%>%
-  dplyr::mutate (variable = 'imd', .before=1)
-
-
-region <- DWMP_weight_f(dt_2019,region)%>%
-  dplyr::mutate (variable = 'region', .before=1)
-
-
-DWMP_weight <- sex %>% 
-  bind_rows(age, eth, imd, region)
-
-
-summary_2019 <- universal_weight %>% 
-  left_join(DWMP_weight, by = c("variable", "group"))
-
-
-### T2D 2019
-#### 
-T2D_2019 <- dt_2019 %>% 
-  dplyr::filter(comorbid_diabetes_t2 == TRUE)
-
-
-sex <-  universal_weight_f(T2D_2019,sex) %>%
-  dplyr::mutate (variable = 'sex', .before=1)
-
-
-age <- universal_weight_f(T2D_2019,age_group_2)%>%
-  dplyr::mutate (variable = 'age', .before=1)
-
-eth <- universal_weight_f(T2D_2019,eth_16_corrected)%>%
-  dplyr::mutate (variable = 'eth', .before=1)
-
-imd <- universal_weight_f(T2D_2019,imd)%>%
-  dplyr::mutate (variable = 'imd', .before=1)
-
-
-region <- universal_weight_f(T2D_2019,region)%>%
-  dplyr::mutate (variable = 'region', .before=1)
-
-
-universal_weight <- sex %>% 
-  bind_rows(age, eth, imd, region)
-
-
-###
-sex <-  DWMP_weight_f(T2D_2019,sex) %>%
-  dplyr::mutate (variable = 'sex', .before=1)
-
-
-age <- DWMP_weight_f(T2D_2019,age_group_2)%>%
-  dplyr::mutate (variable = 'age', .before=1)
-
-eth <- DWMP_weight_f(T2D_2019,eth_16_corrected)%>%
-  dplyr::mutate (variable = 'eth', .before=1)
-
-imd <- DWMP_weight_f(T2D_2019,imd)%>%
-  dplyr::mutate (variable = 'imd', .before=1)
-
-
-region <- DWMP_weight_f(T2D_2019,region)%>%
-  dplyr::mutate (variable = 'region', .before=1)
-
-
-DWMP_weight <- sex %>% 
-  bind_rows(age, eth, imd, region)
-
-
-T2D_2019 <- universal_weight %>% 
-  left_join(DWMP_weight, by = c("variable", "group"))
-
-
-######## T1D_2019
-
-
-T1D_2019 <- dt_2019 %>% 
-  dplyr::filter(comorbid_diabetes_t1 == TRUE)
-
-
-
-
-sex <-  universal_weight_f(T1D_2019,sex) %>%
-  dplyr::mutate (variable = 'sex', .before=1)
-
-
-age <- universal_weight_f(T1D_2019,age_group_2)%>%
-  dplyr::mutate (variable = 'age', .before=1)
-
-eth <- universal_weight_f(T1D_2019,eth_16_corrected)%>%
-  dplyr::mutate (variable = 'eth', .before=1)
-
-imd <- universal_weight_f(T1D_2019,imd)%>%
-  dplyr::mutate (variable = 'imd', .before=1)
-
-
-region <- universal_weight_f(T1D_2019,region)%>%
-  dplyr::mutate (variable = 'region', .before=1)
-
-
-universal_weight <- sex %>% 
-  bind_rows(age, eth, imd, region)
-
-
-
-
-
-
-
-
-
-sex <-  DWMP_weight_f(T1D_2019,sex) %>%
-  dplyr::mutate (variable = 'sex', .before=1)
-
-
-age <- DWMP_weight_f(T1D_2019,age_group_2)%>%
-  dplyr::mutate (variable = 'age', .before=1)
-
-eth <- DWMP_weight_f(T1D_2019,eth_16_corrected)%>%
-  dplyr::mutate (variable = 'eth', .before=1)
-
-imd <- DWMP_weight_f(T1D_2019,imd)%>%
-  dplyr::mutate (variable = 'imd', .before=1)
-
-
-region <- DWMP_weight_f(T1D_2019,region)%>%
-  dplyr::mutate (variable = 'region', .before=1)
-
-
-DWMP_weight <- sex %>% 
-  bind_rows(age, eth, imd, region)
-
-
-T1D_2019 <- universal_weight %>% 
-  left_join(DWMP_weight, by = c("variable", "group"))
-
-
-### Hypertension_2019
-
-BP_2019 <- dt_2019 %>% 
-  dplyr::filter(comorbid_hypertension == TRUE)
-
-
-
-
-sex <-  universal_weight_f(BP_2019,sex) %>%
-  dplyr::mutate (variable = 'sex', .before=1)
-
-
-age <- universal_weight_f(BP_2019,age_group_2)%>%
-  dplyr::mutate (variable = 'age', .before=1)
-
-eth <- universal_weight_f(BP_2019,eth_16_corrected)%>%
-  dplyr::mutate (variable = 'eth', .before=1)
-
-imd <- universal_weight_f(BP_2019,imd)%>%
-  dplyr::mutate (variable = 'imd', .before=1)
-
-
-region <- universal_weight_f(BP_2019,region)%>%
-  dplyr::mutate (variable = 'region', .before=1)
-
-
-universal_weight <- sex %>% 
-  bind_rows(age, eth, imd, region)
-
-
-
-
-
-
-
-
-
-sex <-  DWMP_weight_f(BP_2019,sex) %>%
-  dplyr::mutate (variable = 'sex', .before=1)
-
-
-age <- DWMP_weight_f(BP_2019,age_group_2)%>%
-  dplyr::mutate (variable = 'age', .before=1)
-
-eth <- DWMP_weight_f(BP_2019,eth_16_corrected)%>%
-  dplyr::mutate (variable = 'eth', .before=1)
-
-imd <- DWMP_weight_f(BP_2019,imd)%>%
-  dplyr::mutate (variable = 'imd', .before=1)
-
-
-region <- DWMP_weight_f(BP_2019,region)%>%
-  dplyr::mutate (variable = 'region', .before=1)
-
-
-DWMP_weight <- sex %>% 
-  bind_rows(age, eth, imd, region)
-
-
-BP_2019 <- universal_weight %>% 
-  left_join(DWMP_weight, by = c("variable", "group"))
+write_csv (summary_2021, here::here ("output/data","Ethnicity_corrected_DWMP_2021.csv"))
